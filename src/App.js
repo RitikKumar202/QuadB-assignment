104cf71cade723e73c0cee4422c18858d8b97a2b
RitikKumar202 <ritik2002july@gmail.com>
2023-08-08 11:15:59 +0530

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import MovieList from "./components/MovieList";
+import MovieDetail from "./components/MovieDetail";
+import Navbar from "./components/Navbar";
+import BookMovieTicket from "./components/BookMovieTicket";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/shows/:id" element={<MovieDetail />} />
+        <Route path="/book/:id" element={<BookMovieTicket />} />
+      </Routes>
     </div>
   );
 }
